f86250fe0e531f1f9e713443d0563d36a358a7e3
joshijayesh <jayeshjo1@utexas.edu>
2017-04-24 16:17:56 -0500

    Updated CuDNN status messages

--- dellve_cudnn_benchmark/include/CuDNN/Status.hpp
+++ dellve_cudnn_benchmark/include/CuDNN/Status.hpp
@@ -13,29 +13,45 @@ namespace CuDNN {
     	Exception(Status status) : mStatus(status) {}
       
 		const char* what() const noexcept {
+            std::string msg = "";
 			switch (mStatus) {
 				case CUDNN_STATUS_SUCCESS:
-					return "PyCuDNN.Exception: CUDNN_STATUS_SUCCESS";
+					return "CuDNN.Exception: CUDNN_STATUS_SUCCESS\n"
+                           "CuDNN Successfully Completed Operation";
 				case CUDNN_STATUS_NOT_INITIALIZED:
-					return "PyCuDNN.Exception: CUDNN_STATUS_NOT_INITIALIZED";
+					return "CuDNN.Exception: CUDNN_STATUS_NOT_INITIALIZED\n"
+                           "Ensure all parameters are initialized before calling function";
 				case CUDNN_STATUS_ALLOC_FAILED:
-					return "PyCuDNN.Exception: CUDNN_STATUS_ALLOC_FAILED";
+					return "CuDNN.Exception: CUDNN_STATUS_ALLOC_FAILED\n"
+                           "Ensure GPU has enough space for the memory alloc requested";
 				case CUDNN_STATUS_BAD_PARAM:
-					return "PyCuDNN.Exception: CUDNN_STATUS_BAD_PARAM";
+					return "CuDNN.Exception: CUDNN_STATUS_BAD_PARAM\n"
+                           "Ensure that all parameters being passed have valid values";
 				case CUDNN_STATUS_ARCH_MISMATCH:
-					return "PyCuDNN.Exception: CUDNN_STATUS_ARCH_MISMATCH";
+					return "CuDNN.Exception: CUDNN_STATUS_ARCH_MISMATCH\n"
+                           "Compile and run the application on a device with appropriate"
+                           " compute capability";
 				case CUDNN_STATUS_MAPPING_ERROR:
-					return "PyCuDNN.Exception: CUDNN_STATUS_MAPPING_ERROR";
+					return "CuDNN.Exception: CUDNN_STATUS_MAPPING_ERROR\n"
+                           "Prior to function call, unbind any previously bound textures";
 				case CUDNN_STATUS_EXECUTION_FAILED:
-					return "PyCuDNN.Exception: CUDNN_STATUS_EXECUTION_FAILED";
+					return "CuDNN.Exception: CUDNN_STATUS_EXECUTION_FAILED\n"
+                           "Check the hardware, an appropriate version of driver, and the"
+                           " cuDNN library are correctly installed"; 
 				case CUDNN_STATUS_INTERNAL_ERROR:
-					return "PyCuDNN.Exception: CUDNN_STATUS_INTERNAL_ERROR";
+					return "CuDNN.Exception: CUDNN_STATUS_INTERNAL_ERROR\n"
+                           "Internal cuDNN operation failed.";
 				case CUDNN_STATUS_NOT_SUPPORTED:
-					return "PyCuDNN.Exception: CUDNN_STATUS_NOT_SUPPORTED";
+					return "CuDNN.Exception: CUDNN_STATUS_NOT_SUPPORTED\n"
+                           "Requested functionality is not supported in cuDNN. Refer to the"
+                           " cuDNN docs to find version issues.";
 				case CUDNN_STATUS_LICENSE_ERROR:
-					return "PyCuDNN.Exception: CUDNN_STATUS_LICENSE_ERROR";
+					return "CuDNN.Exception: CUDNN_STATUS_LICENSE_ERROR\n"
+                           "Error can occur if requested functionality requires a license and"
+                           " the license is not present or expired of if NVIDIA_LICENSE_FILE env"
+                           " is not set properly";
 				default:
-					return "PyCuDNN.Exception: CUDNN_STATUS_UNKNOWN";
+					return "CuDNN.Exception: CUDNN_STATUS_UNKNOWN";
 			};
 		};
 
