28ded0dda9f6a19ae6ce8a038311fe3c02e1f908
Kian Baula <qdbaula@utexas.edu>
2017-04-25 05:33:33 -0500

    Update .gitignore and add helper function to query GPU names

--- dellve_cudnn/stresstools/helper/gpu_info.py
+++ dellve_cudnn/stresstools/helper/gpu_info.py
@@ -1,5 +1,21 @@
 
 from pynvml import *
+# import dellve_cudnn_benchmark as dcb
+
+def get_names():
+    names = []
+
+    nvmlInit()
+    device_count = nvmlDeviceGetCount()
+    for i in range(device_count):
+        #TODO: implement a helper function in dcb that checks 
+        #      if GPU architecture is supported
+        #if dcb.valid_gpu(i):
+        handle = nvmlDeviceGetHandleByIndex(i)
+        names.append('Device {} : {}'.format(i, nvmlDeviceGetName(handle)))
+    nvmlShutdown()
+
+    return names
 
 def get_total_mem(device_id):
     nvmlInit()
