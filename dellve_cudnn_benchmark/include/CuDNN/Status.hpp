033f5e6476c692eb652977184348286116af39e8
Konstantyn Komarov <komarov.konstant@utexas.edu>
2017-04-25 01:05:17 -0500

    Fixing compile issues

--- dellve_cudnn_benchmark/include/CuDNN/Status.hpp
+++ dellve_cudnn_benchmark/include/CuDNN/Status.hpp
@@ -30,42 +30,42 @@ namespace CuDNN {
                                (mLine.size() ? (" " + mLine) : "");
 			switch (mStatus) {
 				case CUDNN_STATUS_SUCCESS:
-					return "CuDNN.Exception: CUDNN_STATUS_SUCCESS" + info + 
-                           "\nCuDNN Successfully Completed Operation";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_SUCCESS" + info + 
+                           "\nCuDNN Successfully completed operation").c_str();
 				case CUDNN_STATUS_NOT_INITIALIZED:
-					return "CuDNN.Exception: CUDNN_STATUS_NOT_INITIALIZED" + info + 
-                           "\nEnsure all parameters are initialized before calling function";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_NOT_INITIALIZED" + info + 
+                           "\nEnsure all parameters are initialized before calling function").c_str();
 				case CUDNN_STATUS_ALLOC_FAILED:
-					return "CuDNN.Exception: CUDNN_STATUS_ALLOC_FAILED" + info + 
-                           "\nEnsure GPU has enough space for the memory alloc requested";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_ALLOC_FAILED" + info + 
+                           "\nEnsure GPU has enough space for the memory alloc requested").c_str();
 				case CUDNN_STATUS_BAD_PARAM:
-					return "CuDNN.Exception: CUDNN_STATUS_BAD_PARAM" + info + 
-                           "\nEnsure that all parameters being passed have valid values";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_BAD_PARAM" + info + 
+                           "\nEnsure that all parameters being passed have valid values").c_str();
 				case CUDNN_STATUS_ARCH_MISMATCH:
-					return "CuDNN.Exception: CUDNN_STATUS_ARCH_MISMATCH" + info + 
+					return std::string("CuDNN.Exception: CUDNN_STATUS_ARCH_MISMATCH" + info + 
                            "\nCompile and run the application on a device with appropriate"
-                           " compute capability";
+                           " compute capability").c_str();
 				case CUDNN_STATUS_MAPPING_ERROR:
-					return "CuDNN.Exception: CUDNN_STATUS_MAPPING_ERROR" + info + 
-                           "\nPrior to function call, unbind any previously bound textures";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_MAPPING_ERROR" + info + 
+                           "\nPrior to function call, unbind any previously bound textures").c_str();
 				case CUDNN_STATUS_EXECUTION_FAILED:
-					return "CuDNN.Exception: CUDNN_STATUS_EXECUTION_FAILED" + info + 
+					return std::string("CuDNN.Exception: CUDNN_STATUS_EXECUTION_FAILED" + info + 
                            "\nCheck the hardware, an appropriate version of driver, and the"
-                           " cuDNN library are correctly installed"; 
+                           " cuDNN library are correctly installed").c_str(); 
 				case CUDNN_STATUS_INTERNAL_ERROR:
-					return "CuDNN.Exception: CUDNN_STATUS_INTERNAL_ERROR" + info + 
-                           "\nInternal cuDNN operation failed.";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_INTERNAL_ERROR" + info + 
+                           "\nInternal cuDNN operation failed.").c_str();
 				case CUDNN_STATUS_NOT_SUPPORTED:
-					return "CuDNN.Exception: CUDNN_STATUS_NOT_SUPPORTED" + info + 
+					return std::string("CuDNN.Exception: CUDNN_STATUS_NOT_SUPPORTED" + info + 
                            "\nRequested functionality is not supported in cuDNN. Refer to the"
-                           " cuDNN docs to find version issues.";
+                           " cuDNN docs to find version issues.").c_str();
 				case CUDNN_STATUS_LICENSE_ERROR:
-					return "CuDNN.Exception: CUDNN_STATUS_LICENSE_ERROR" + info + 
+					return std::string("CuDNN.Exception: CUDNN_STATUS_LICENSE_ERROR" + info + 
                            "\nError can occur if requested functionality requires a license and"
                            " the license is not present or expired of if NVIDIA_LICENSE_FILE env"
-                           " is not set properly";
+                           " is not set properly").c_str();
 				default:
-					return "CuDNN.Exception: CUDNN_STATUS_UNKNOWN";
+					return std::string("CuDNN.Exception: CUDNN_STATUS_UNKNOWN").c_str();
 			};
 		};
 
