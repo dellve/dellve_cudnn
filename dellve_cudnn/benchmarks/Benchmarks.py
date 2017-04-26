18c98583c85aea370b6e88b2b1d4811345c6151f
qdbaula@utexas.edu <jayeshjo1@utexas.edu>
2017-04-25 22:16:04 -0500

    Adding descriptions for benchmarks

--- dellve_cudnn/benchmarks/Benchmarks.py
+++ dellve_cudnn/benchmarks/Benchmarks.py
@@ -6,6 +6,15 @@ from helper import problem_set as ps
 class ForwardActivationBenchmark(BenchmarkFactory):
     name = 'ForwardActivationBenchmark'
     csv_filename = 'activation/basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the activation operation in cuDNN library with forward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of each feature map\n"
+                   "H = height of each feature map\n"
+                   "C = number of feature maps per image\n"
+                   "N = number of feature maps"
+                  )
 
     def get_controller(self):
         return dcb.activation_forward
@@ -19,6 +28,16 @@ class ForwardActivationBenchmark(BenchmarkFactory):
 class BackwardActivationBenchmark(BenchmarkFactory):
     name = 'BackwardActivationBenchmark'
     csv_filename = 'activation/basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the activation operation in cuDNN library with backward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of each feature map\n"
+                   "H = height of each feature map\n"
+                   "C = number of feature maps per image\n"
+                   "N = number of feature maps"
+                  )
+
 
     def get_controller(self):
         return dcb.activation_forward
@@ -31,6 +50,16 @@ class BackwardActivationBenchmark(BenchmarkFactory):
 
 class ForwardSoftmaxBenchmark(BenchmarkFactory):
     name = 'ForwardSoftmaxBenchmark'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the softmax operation in cuDNN library with forward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of each feature map\n"
+                   "H = height of each feature map\n"
+                   "C = number of feature maps per image\n"
+                   "N = number of feature maps"
+                  )
+
     csv_filename = 'softmax/basic.csv'
 
     def get_controller(self):
@@ -50,6 +79,16 @@ class ForwardSoftmaxBenchmark(BenchmarkFactory):
 class BackwardSoftmaxBenchmark(BenchmarkFactory):
     name = 'BackwardSoftmaxBenchmark'
     csv_filename = 'softmax/basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the softmax operation in cuDNN library with backward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of each feature map\n"
+                   "H = height of each feature map\n"
+                   "C = number of feature maps per image\n"
+                   "N = number of feature maps"
+                  )
+
 
     def get_controller(self):
         return dcb.softmax_backward
@@ -66,8 +105,24 @@ class BackwardSoftmaxBenchmark(BenchmarkFactory):
         return header
 
 class ForwardPoolingBenchmark(BenchmarkFactory):
+    # TODO: override config to set algorithm
     name = 'ForwardPoolingBenchmark'
     csv_filename = 'pooling/basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the pooling operation in cuDNN library with forward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of input image\n"
+                   "H = height of input image\n"
+                   "C = number of channels per image\n"
+                   "N = number of images\n"
+                   "WIN_H = height of pooling window\n"
+                   "WIN_W = width of pooling window\n"
+                   "PAD_H = height of the zero padding\n"
+                   "PAD_W = width of the zero padding\n"
+                   "VSTRIDE = pooling vertical stride\n"
+                   "HSTRIDE = pooling horizontal stride"
+                  )
 
     def get_controller(self):
         return dcb.pooling_forward
@@ -86,6 +141,21 @@ class ForwardPoolingBenchmark(BenchmarkFactory):
 class BackwardPoolingBenchmark(BenchmarkFactory):
     name = 'BackwardPoolingBenchmark'
     csv_filename = 'pooling/basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the pooling operation in cuDNN library with backward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of each feature map\n"
+                   "H = height of each feature map\n"
+                   "C = number of feature maps per image\n"
+                   "N = number of feature maps\n"
+                   "WIN_H = height of pooling window\n"
+                   "WIN_W = width of pooling window\n"
+                   "PAD_H = height of the 0 padding\n"
+                   "PAD_W = width of the 0 padding\n"
+                   "VSTRIDE = pooling vertical stride\n"
+                   "HSTRIDE = pooling horizontal stride"
+                  )
 
     def get_controller(self):
         return dcb.pooling_backward
@@ -104,6 +174,22 @@ class BackwardPoolingBenchmark(BenchmarkFactory):
 class ForwardConvolutionBenchmark(BenchmarkFactory):
     name = 'ForwardConvolutionBenchmark'
     csv_filename = 'convolution/forward_basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the convolution operation in cuDNN library with forward\n"
+                   "propagation.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of input image\n"
+                   "H = height of input image\n"
+                   "C = number of channels per input image\n"
+                   "N = number of input images\n"
+                   "K = number of filters\n"
+                   "R = height of filter\n"
+                   "S = width of filter\n"
+                   "PAD_H = height of the zero padding\n"
+                   "PAD_W = width of the zero padding\n"
+                   "VSTRIDE = filter vertical stride\n"
+                   "HSTRIDE = filter horizontal stride"
+                  )
 
     def get_controller(self):
         return dcb.convolution_forward
@@ -117,6 +203,22 @@ class ForwardConvolutionBenchmark(BenchmarkFactory):
 class BackwardConvolutionDataBenchmark(BenchmarkFactory):
     name = 'BackwardConvolutionDataBenchmark'
     csv_filename = 'convolution/backward_data_basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the convolution operation in cuDNN library with backward\n"
+                   "propagation in data.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of input image\n"
+                   "H = height of input image\n"
+                   "C = number of channels per input image\n"
+                   "N = number of input images\n"
+                   "K = number of filters\n"
+                   "R = height of filter\n"
+                   "S = width of filter\n"
+                   "PAD_H = height of the zero padding\n"
+                   "PAD_W = width of the zero padding\n"
+                   "VSTRIDE = filter vertical stride\n"
+                   "HSTRIDE = filter horizontal stride"
+                  )
 
     def get_controller(self):
         return dcb.convolution_backward_data
@@ -130,6 +232,22 @@ class BackwardConvolutionDataBenchmark(BenchmarkFactory):
 class BackwardConvolutionFilterBenchmark(BenchmarkFactory):
     name = 'BackwardConvolutionFilterBenchmark'
     csv_filename = 'convolution/backward_filter_basic.csv'
+    description = ("Benchmarks the GPU for different configurations testing\n"
+                   "the convolution operation in cuDNN library with backward\n"
+                   "propagation in filter.\n\n"
+                   "----------------Parameter Description------------------\n"
+                   "W = width of input image\n"
+                   "H = height of input image\n"
+                   "C = number of channels per input image\n"
+                   "N = number of input images\n"
+                   "K = number of filters\n"
+                   "R = height of filter\n"
+                   "S = width of filter\n"
+                   "PAD_H = height of the zero padding\n"
+                   "PAD_W = width of the zero padding\n"
+                   "VSTRIDE = filter vertical stride\n"
+                   "HSTRIDE = filter horizontal stride"
+                  )
 
     def get_controller(self):
         return dcb.convolution_backward_filter
