3af4bac06fe869ad1aa6cac35952a3152f63f800
qdbaula@utexas.edu <jayeshjo1@utexas.edu>
2017-05-02 11:04:26 -0500

    Revert setup.

--- setup.py
+++ setup.py
@@ -6,14 +6,10 @@ import setuptools
 import os
 import os.path
 
-if(os.environ.has_key('CUDA_PATH')):
-    cuda_path       = os.environ['CUDA_PATH']
-    cudnn_path      = os.environ['CUDNN_PATH']
-else:
-    cuda_path       = ''
-    cudnn_path      = ''
+cuda_path       = os.environ['CUDA_PATH']
 cuda_include    = os.path.join(cuda_path, "include")
 cuda_lib64      = os.path.join(cuda_path, "lib64")
+cudnn_path      = os.environ['CUDNN_PATH']
 cudnn_include   = os.path.join(cudnn_path, "include")
 cudnn_lib64     = os.path.join(cudnn_path, "lib64")
 
@@ -31,66 +27,63 @@ class get_pybind_include(object):
         import pybind11
         return pybind11.get_include(self.user)
 
-if(cuda_path is not ''):
-    ext_modules = [
-        Extension('dellve_cudnn_benchmark',
-            sources=[
-                'dellve_cudnn_benchmark/src/dellve_cudnn_benchmark.cpp',
-                ],
-            include_dirs=[
-                'dellve_cudnn_benchmark/include/',
-                cuda_include,
-                cudnn_include,
-                get_pybind_include(),
-                get_pybind_include(user=True),
-                ],
-            libraries = [
-                'cuda',
-                'cudart',
-                'cudnn',
-                'curand'
-                ],
-            library_dirs=[
-                cuda_lib64,
-                cudnn_lib64
-                ],
-            extra_compile_args=[
-                '-std=c++11',
-                '-std=c++1y'
-                ],
-            language='c++'
-        ),
+ext_modules = [
+    Extension('dellve_cudnn_benchmark',
+        sources=[
+            'dellve_cudnn_benchmark/src/dellve_cudnn_benchmark.cpp',
+            ],
+        include_dirs=[
+            'dellve_cudnn_benchmark/include/',
+            cuda_include,
+            cudnn_include,
+            get_pybind_include(),
+            get_pybind_include(user=True),
+            ],
+        libraries = [
+            'cuda',
+            'cudart',
+            'cudnn',
+            'curand'
+            ],
+        library_dirs=[
+            cuda_lib64,
+            cudnn_lib64
+            ],
+        extra_compile_args=[
+            '-std=c++11',
+            '-std=c++1y'
+            ],
+        language='c++'
+    ),
 
-        Extension('dellve_cudnn_helper',
-            sources=[
-                'dellve_cudnn_benchmark/src/dellve_cudnn_helper.cpp',
-                ],
-            include_dirs=[
-                'dellve_cudnn_benchmark/include/',
-                cuda_include,
-                cudnn_include,
-                get_pybind_include(),
-                get_pybind_include(user=True),
-                ],
-            libraries = [
-                'cuda',
-                'cudart',
-                'cudnn',
-                'curand'
-                ],
-            library_dirs=[
-                cuda_lib64,
-                cudnn_lib64
-                ],
-            extra_compile_args=[
-                '-std=c++11',
-                '-std=c++1y'
-                ],
-            language='c++'
-        ),
-    ]
-else:
-    ext_modules = []
+    Extension('dellve_cudnn_helper',
+        sources=[
+            'dellve_cudnn_benchmark/src/dellve_cudnn_helper.cpp',
+            ],
+        include_dirs=[
+            'dellve_cudnn_benchmark/include/',
+            cuda_include,
+            cudnn_include,
+            get_pybind_include(),
+            get_pybind_include(user=True),
+            ],
+        libraries = [
+            'cuda',
+            'cudart',
+            'cudnn',
+            'curand'
+            ],
+        library_dirs=[
+            cuda_lib64,
+            cudnn_lib64
+            ],
+        extra_compile_args=[
+            '-std=c++11',
+            '-std=c++1y'
+            ],
+        language='c++'
+    ),
+]
 
 setup (
     name='dellve_cudnn',
